3bf4c3235e956e1983e6c4d29ae05fdff448b128
Daniel Carvalho <danielborgesdecarvalho@gmail.com>
2021-06-30 00:16:32 -0300

    add async actions with redux thunk

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ import store from './redux/store';
 import './App.css';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import HooksIceCreamContainer from './components/HooksIceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
+import UserContainer from './components/UserContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <UserContainer />
+        {/* <ItemContainer cakeReducer/>
+        <ItemContainer />
         <HooksCakeContainer />
         <HooksIceCreamContainer />
+        <NewCakeContainer /> */}
       </div>
     </Provider>
   );
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux"
 import cakeReducer from "./cake/cakeReducer"
 import iceCreamReducer from "./iceCream/iceCreamReducer"
+import userReducer from "./user/userReducer"
 
 const rootReducer = combineReducers({
   cakeReducer,
-  iceCreamReducer
+  userReducer,
+  iceCreamReducer,
 })
 
 export default rootReducer
